bd1e13414fad46c5d83c1629262f0b3d1ddfa092
sharpMorgrim <vfrc008@icloud.com>
2024-09-23 11:54:50 +0300

    Feat: car service quiz

--- main.py
+++ main.py
@@ -2,36 +2,123 @@ import asyncio
 import logging
 import sys
 from os import getenv
-from typing import Any, Dict
+from typing import Any, List
 
-from aiogram import Bot, Dispatcher, F, Router, html
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
+from aiogram import Bot, Dispatcher, F, Router
 from aiogram.filters import Command, CommandStart
 from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.scene import Scene, on, SceneRegistry
+from aiogram.fsm.storage.memory import SimpleEventIsolation
 from aiogram.types import (
     KeyboardButton,
     Message,
-    ReplyKeyboardMarkup,
     ReplyKeyboardRemove,
 )
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 TOKEN = getenv("BOT_TOKEN")
-
-form_router = Router()
-
-
-class Car(StatesGroup):
-    car_type = State()
 
 
 car_types = ["легковая", "внедорожник", "минивен", "говновоз"]
 
+services = [
+    "полировка", "хим чистка",  "полировка стекла", "тонировка",
+    "защитные покрытия", "оклейка кузова полиуретановой пленкой"
+]
 
-@form_router.message(CommandStart())
+option_list = {
+    "polirovka": {
+        "legkaya": {"легковая": 1000, "внедорожник": 2500, "минивен": 1700, "говновоз": 9999},
+        "glubokaya": {"легковая": 2000, "внедорожник": 5000, "минивен": 3200, "говновоз": 9999}
+    },
+    "xim chistka": {
+        "all": {"легковая": 10000, "внедорожник": 25000, "минивен": 17000, "говновоз": 99999},
+        "1 chair": {"легковая": 1000, "внедорожник": 2500, "минивен": 1700, "говновоз": 9999}
+    },
+}
+
+
+class GTTScene(Scene, state="quiz"):
+    @on.message.enter()
+    async def on_enter(
+            self, message: Message, state: FSMContext,
+            step: str | None = "car_choosing", path: List | None = None) -> Any:
+
+        if step == "car_choosing":
+            text = "Выберите тип машины"
+            options = car_types
+        elif step == "service_choosing":
+            text = "Выберите услугу"
+            await state.update_data(path=path)
+
+            if path is None:
+                path = []
+
+            options = option_list
+            for option in path:
+                options = options[option]
+
+            if list(options.keys()) == car_types:
+                data = await state.get_data()
+                answers = data.get("answers", {})
+                answers[path[0]] = options[data["answers"]["car_choosing"]]
+                data["path"] = []
+
+                await state.update_data(data=data)
+                options = option_list
+
+            options = list(options.keys()) + ["Завершить выбор услуг"]
+
+        markup = ReplyKeyboardBuilder()
+        markup.add(*[KeyboardButton(text=option) for option in options])
+
+        await state.update_data(step=step)
+        return await message.answer(text=text, reply_markup=markup.adjust(2).as_markup(resize_keyboard=True))
+
+    @on.message.exit()
+    async def on_exit(self, message: Message, state: FSMContext) -> None:
+        pass
+
+    @on.message(F.text)
+    async def answer(self, message: Message, state: FSMContext) -> None:
+        data = await state.get_data()
+        step = data["step"]
+        path = data.get("path")
+
+        if step == "car_choosing":
+            if message.text.casefold() in car_types:
+                answers = data.get("answers", {})
+                answers[step] = message.text
+
+                await state.update_data(answers=answers)
+                step = "service_choosing"
+            else:
+                await message.answer("Не известный тип автомобиля, попробуйте еще раз")
+        elif step == "service_choosing":
+            if path is None:
+                options = option_list.keys()
+            else:
+                options = option_list
+                for option in path:
+                    options = options[option]
+
+            if message.text.casefold() in options:
+                if path is None:
+                    path = []
+                path.append(message.text)
+        await self.wizard.retake(step=step, path=path)
+
+    @on.message()
+    async def unknown_message(self, message: Message) -> None:
+        await message.answer("Please select an answer.")
+
+
+quiz_router = Router(name=__name__)
+quiz_router.message.register(GTTScene.as_handler(), Command("new_application"))
+
+
+@quiz_router.message(CommandStart())
 async def command_start(message: Message, state: FSMContext) -> None:
-    await state.set_state(Car.car_type)
     await message.answer(
         "Добро пожаловать в бот GTT delailing!"
         "Бот воздан для улучшения работы с нашими уважаемыми клиентами! (с вами)."
@@ -42,53 +129,21 @@ async def command_start(message: Message, state: FSMContext) -> None:
     )
 
 
-@form_router.message(Command("new_application"))
-async def new_application(message: Message, state: FSMContext) -> None:
-    await state.set_state(Car.car_type)
-    await message.answer(
-        "Ваше заявление созданно! Пожалуйста, выберете тип вашей машины",
-        reply_markup=ReplyKeyboardMarkup(
-            keyboard=[[KeyboardButton(text=car.capitalize()) for car in car_types]],
-            resize_keyboard=True)
+def create_dispatcher():
+    dispatcher = Dispatcher(
+        events_isolation=SimpleEventIsolation(),
     )
+    dispatcher.include_router(quiz_router)
 
+    scene_registry = SceneRegistry(dispatcher)
+    scene_registry.add(GTTScene)
 
-@form_router.message(Car.car_type)
-async def car(message: Message, state: FSMContext) -> None:
-    if message.text.casefold() in car_types:
-        await message.answer(f"Вы выбрали {message.text.casefold().capitalize()}")
-    else:
-        await state.set_state(Car.car_type)
-        await message.answer(
-            f"Не известный тип автомобиля, попробуйте еще раз",
-            reply_markup=ReplyKeyboardMarkup(
-                keyboard=[[KeyboardButton(text=car.capitalize()) for car in car_types]],
-                resize_keyboard=True
-            )
-        )
-
-
-async def show_summary(message: Message, data: Dict[str, Any], positive: bool = True) -> None:
-    name = data["name"]
-    language = data.get("language", "<something unexpected>")
-    text = f"I'll keep in mind that, {html.quote(name)}, "
-    text += (
-        f"you like to write bots with {html.quote(language)}."
-        if positive
-        else "you don't like to write bots, so sad..."
-    )
-    await message.answer(text=text, reply_markup=ReplyKeyboardRemove())
+    return dispatcher
 
 
 async def main():
-    # Initialize Bot instance with default bot properties which will be passed to all API calls
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
-    dp = Dispatcher()
-
-    dp.include_router(form_router)
-
-    # Start event dispatching
+    dp = create_dispatcher()
+    bot = Bot(token=TOKEN)
     await dp.start_polling(bot)
 
 
